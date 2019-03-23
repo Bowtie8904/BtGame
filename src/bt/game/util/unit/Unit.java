7cb869c3e617da42e1de7b6676670ec3b4f8def2
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-03-23 18:18:33 +0100

    Added comments.

--- src/bt/game/util/unit/Unit.java
+++ src/bt/game/util/unit/Unit.java
@@ -1,8 +1,10 @@
 package bt.game.util.unit;
 
 /**
+ * A measurement conversion class between pixels and custom game units. This class uses a set ratio of pixels per unit
+ * to do its calculations.
+ * 
  * @author &#8904
- *
  */
 public class Unit
 {
@@ -10,15 +12,23 @@ public class Unit
     private float pixels;
     private float units;
 
+    /**
+     * Sets the pixel per unit ratio. The given value is the amount of pixels one unit should consist of. If this method
+     * is not called, the default ratio of 1 will be used.
+     * 
+     * @param pixelsPerUnit
+     */
     public static void setRatio(float pixelsPerUnit)
     {
-        if (pixelsPerUnit <= 0)
-        {
-            // throw new IllegalArgumentException("A unit can't consist of zero or less pixels.");
-        }
         Unit.ratio = pixelsPerUnit;
     }
 
+    /**
+     * Creates a new instance where the given value is treated as units.
+     * 
+     * @param units
+     * @return
+     */
     public static Unit forUnits(float units)
     {
         Unit unit = new Unit();
@@ -26,6 +36,12 @@ public class Unit
         return unit;
     }
 
+    /**
+     * Creates a new instance where the given value is treated as pixels.
+     * 
+     * @param pixels
+     * @return
+     */
     public static Unit forPixels(float pixels)
     {
         Unit unit = new Unit();
@@ -33,33 +49,63 @@ public class Unit
         return unit;
     }
 
+    /**
+     * Adds the given amount of units to this instances units.
+     * 
+     * @param units
+     */
     public void addUnits(float units)
     {
         setUnits(this.units + units);
     }
 
+    /**
+     * Adds the given amount of pixels to this instances pixels.
+     * 
+     * @param pixels
+     */
     public void addPixels(float pixels)
     {
         setPixels(this.pixels + pixels);
     }
 
-    protected void setUnits(float units)
+    /**
+     * Sets the units of this instance to the given value. The pixels will be calculated with the set ratio.
+     * 
+     * @param units
+     */
+    public void setUnits(float units)
     {
         this.units = units;
         this.pixels = units * Unit.ratio;
     }
 
-    protected void setPixels(float pixels)
+    /**
+     * Sets the pixels of this instance to the given value. The units will be calculated with the set ratio.
+     * 
+     * @param units
+     */
+    public void setPixels(float pixels)
     {
         this.pixels = pixels;
         this.units = pixels / Unit.ratio;
     }
 
+    /**
+     * Gets the value for units this instance holds.
+     * 
+     * @return
+     */
     public float units()
     {
         return this.units;
     }
 
+    /**
+     * Gets the value for pixels this instance holds.
+     * 
+     * @return
+     */
     public float pixels()
     {
         return this.pixels;
