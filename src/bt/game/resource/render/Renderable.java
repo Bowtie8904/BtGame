1a02601cfe93a6cd6be6a919e5b55b877b38af90
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-03-24 14:55:09 +0100

    Added comments.

--- src/bt/game/resource/render/Renderable.java
+++ src/bt/game/resource/render/Renderable.java
@@ -6,10 +6,21 @@ import bt.game.util.unit.Unit;
 import bt.runtime.Killable;
 
 /**
+ * Defines a class which can be rendered.
+ * 
  * @author &#8904
- *
  */
 public interface Renderable extends Killable
 {
+    /**
+     * Renders this instance at the position x|y with a width of w and a height of h. The given Graphics object is used
+     * to do the drawing.
+     * 
+     * @param g
+     * @param x
+     * @param y
+     * @param w
+     * @param h
+     */
     public void render(Graphics g, Unit x, Unit y, Unit w, Unit h);
 }
