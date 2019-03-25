42f9d7cc42ee74e80f6b46be95be2d14ed9d9ce4
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-03-25 23:08:36 +0100

    Now extends RenderableImage rather that copying its code.

--- src/bt/game/resource/render/impl/RenderableGif.java
+++ src/bt/game/resource/render/impl/RenderableGif.java
@@ -1,41 +1,15 @@
 package bt.game.resource.render.impl;
 
-import java.awt.Graphics;
-import java.awt.Image;
-
 import javax.swing.ImageIcon;
-
-import bt.game.resource.render.Renderable;
-import bt.game.util.unit.Unit;
 
 /**
  * @author &#8904
  *
  */
-public class RenderableGif implements Renderable
+public class RenderableGif extends RenderableImage
 {
-    private Image image;
-
     public RenderableGif(ImageIcon image)
     {
-        this.image = image.getImage();
-    }
-
-    /**
-     * @see bt.game.resource.render.Renderable#render(java.awt.Graphics, bt.game.util.unit.Unit, bt.game.util.unit.Unit)
-     */
-    @Override
-    public void render(Graphics g, Unit x, Unit y, Unit w, Unit h)
-    {
-        g.drawImage(image, (int)x.pixels(), (int)y.pixels(), (int)w.pixels(), (int)h.pixels(), null);
-    }
-
-    /**
-     * @see bt.runtime.Killable#kill()
-     */
-    @Override
-    public void kill()
-    {
-        this.image.flush();
+        super(image.getImage());
     }
 }
