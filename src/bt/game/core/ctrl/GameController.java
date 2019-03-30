3d22973b4299dc911fb5df506bf377d9b15ee9d2
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-03-30 14:00:35 +0100

    Implemented GameController.

---
+++ src/bt/game/core/ctrl/GameController.java
@@ -0,0 +1,162 @@
+package bt.game.core.ctrl;
+
+import java.awt.Component;
+import java.awt.event.KeyEvent;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
+
+import bt.key.KeyAction;
+import bt.key.KeyBoardHook;
+import bt.utils.log.Logger;
+
+/**
+ * A class to handle key and mouse controls. Actions defined in runnables can be defined for each key or mouse button.
+ * 
+ * @author &#8904
+ */
+public class GameController
+{
+    private static GameController instance;
+    private Map<Integer, KeyAction[]> keyMappings;
+
+    public static GameController get()
+    {
+        if (instance == null)
+        {
+            instance = new GameController();
+        }
+
+        return instance;
+    }
+
+    protected GameController()
+    {
+        this.keyMappings = new HashMap<>();
+    }
+
+    /**
+     * Sets the action that is executed when the key with the given key code is pressed without any modifiers (shift,
+     * ctrl or alt). This method will override any action that was set for this specifid condition before.
+     * 
+     * @param keyCode
+     * @param action
+     */
+    public void onKeyPress(int keyCode, Runnable action)
+    {
+        onKeyPress(keyCode, KeyAction.NO_MODIFIER, action);
+    }
+
+    /**
+     * Sets the action that is executed when the key with the given key code is pressed together with the given modifier
+     * ({@link KeyAction#SHIFT_MODIFIER shift}, {@link KeyAction#CTRL_MODIFIER ctrl} or {@link KeyAction#ALT_MODIFIER
+     * alt}). This method will override any action that was set for this specifid condition before.
+     * 
+     * @param keyCode
+     * @param modifier
+     * @param action
+     */
+    public void onKeyPress(int keyCode, int modifier, Runnable action)
+    {
+        KeyAction[] actions = this.keyMappings.get(keyCode);
+
+        if (actions == null)
+        {
+            throw new IllegalArgumentException("No mapping for key code = " + keyCode);
+        }
+
+        actions[modifier].setKeyPressedAction((e) -> action.run());
+    }
+
+    /**
+     * Sets the action that is executed when the key with the given key code is released without any modifiers (shift,
+     * ctrl or alt). This method will override any action that was set for this specifid condition before.
+     * 
+     * @param keyCode
+     * @param action
+     */
+    public void onKeyRelease(int keyCode, Runnable action)
+    {
+        onKeyRelease(keyCode, KeyAction.NO_MODIFIER, action);
+    }
+
+    /**
+     * Sets the action that is executed when the key with the given key code is released together with the given
+     * modifier ({@link KeyAction#SHIFT_MODIFIER shift}, {@link KeyAction#CTRL_MODIFIER ctrl} or
+     * {@link KeyAction#ALT_MODIFIER alt}). This method will override any action that was set for this specifid
+     * condition before.
+     * 
+     * @param keyCode
+     * @param modifier
+     * @param action
+     */
+    public void onKeyRelease(int keyCode, int modifier, Runnable action)
+    {
+        KeyAction[] actions = this.keyMappings.get(keyCode);
+
+        if (actions == null)
+        {
+            throw new IllegalArgumentException("No mapping for key code = " + keyCode);
+        }
+
+        actions[modifier].setKeyReleasedAction((e) -> action.run());
+    }
+
+    public void doInitialMapping(Component comp)
+    {
+        int count = 0;
+        Field[] fields = KeyEvent.class.getDeclaredFields();
+
+        for (Field f : fields)
+        {
+            if (Modifier.isStatic(f.getModifiers())
+                    && Modifier.isPublic(f.getModifiers())
+                    && Modifier.isFinal(f.getModifiers())
+                    && f.getName().startsWith("VK_"))
+            {
+                try
+                {
+                    count ++ ;
+                    int code = f.getInt(null);
+
+                    KeyAction[] actions = new KeyAction[] {
+                            new KeyAction(comp, code, KeyAction.NO_MODIFIER, (e) ->
+                            {
+                            }, (e) ->
+                            {
+                            }),
+                            new KeyAction(comp, code, KeyAction.SHIFT_MODIFIER, (e) ->
+                            {
+                            }, (e) ->
+                            {
+                            }),
+                            new KeyAction(comp, code, KeyAction.ALT_MODIFIER, (e) ->
+                            {
+                            }, (e) ->
+                            {
+                            }),
+                            new KeyAction(comp, code, KeyAction.CTRL_MODIFIER, (e) ->
+                            {
+                            }, (e) ->
+                            {
+                            }),
+                    };
+
+                    this.keyMappings.put(code, actions);
+
+                    for (KeyAction a : actions)
+                    {
+                        KeyBoardHook.get().addKeyAction(a);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Logger.global().print(e);
+                }
+            }
+        }
+
+        Logger.global().print("Mapped " + count + " keys.");
+    }
+}
