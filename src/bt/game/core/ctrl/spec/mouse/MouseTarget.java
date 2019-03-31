473b5b4cfea3f15d19c344f29b2b4ed99518eccd
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-03-31 11:15:24 +0200

    Added MouseTraget interface.

---
+++ src/bt/game/core/ctrl/spec/mouse/MouseTarget.java
@@ -0,0 +1,25 @@
+package bt.game.core.ctrl.spec.mouse;
+
+import bt.game.util.unit.Unit;
+
+/**
+ * @author &#8904
+ */
+public interface MouseTarget
+{
+    public void onClick();
+
+    public void onHover();
+
+    public void afterHover();
+
+    public Unit getX();
+
+    public Unit getY();
+
+    public Unit getZ();
+
+    public Unit getW();
+
+    public Unit getH();
+}
