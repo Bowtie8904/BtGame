fd24aab390247ba42ed611fa26bd3890cbd074ba
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-04-06 00:21:20 +0200

    Added the animated interface to trigger animation setup by the resource loader.

---
+++ src/bt/game/core/obj/intf/Animated.java
@@ -0,0 +1,11 @@
+package bt.game.core.obj.intf;
+
+/**
+ * Hint for the resource loader to setup animations of the implementing object after all resources have been loaded.
+ * 
+ * @author &#8904
+ */
+public interface Animated
+{
+    public void setupAnimations();
+}
