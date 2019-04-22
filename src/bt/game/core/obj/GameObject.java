673c4698be31f3eec7c73911a64931cc75d2d712
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-04-23 00:06:23 +0200

    Made changes to allow interfacing with the dyn4j Body.

--- src/bt/game/core/obj/GameObject.java
+++ src/bt/game/core/obj/GameObject.java
@@ -1,5 +1,8 @@
 package bt.game.core.obj;
 
+import org.dyn4j.dynamics.Body;
+import org.dyn4j.geometry.Vector2;
+
 import bt.game.core.obj.hand.ObjectHandler;
 import bt.game.core.scene.Scene;
 import bt.game.util.unit.Unit;
@@ -10,14 +13,8 @@ import bt.game.util.unit.Unit;
  * 
  * @author &#8904
  */
-public abstract class GameObject
+public abstract class GameObject extends Body
 {
-    /** The unit X position of this object. */
-    protected Unit x;
-
-    /** The unit Y position of this object. */
-    protected Unit y;
-
     /** The unit Z position of this object. */
     protected Unit z;
 
@@ -26,12 +23,6 @@ public abstract class GameObject
 
     /** The unit height of this object. */
     protected Unit h;
-
-    /** The unit velocity on the X axis. */
-    protected Unit xVel;
-
-    /** The unit velocity on the Y axis. */
-    protected Unit yVel;
 
     /** The scene that this object is used in. */
     protected Scene scene;
@@ -51,11 +42,8 @@ public abstract class GameObject
     public GameObject(Scene scene, Unit x, Unit y, Unit z)
     {
         this.scene = scene;
-        this.x = x;
-        this.y = y;
         this.z = z;
-        this.xVel = Unit.forUnits(0);
-        this.yVel = Unit.forUnits(0);
+        translateToOrigin();
     }
 
     /**
@@ -66,26 +54,6 @@ public abstract class GameObject
     public Scene getScene()
     {
         return this.scene;
-    }
-
-    /**
-     * Sets the X position.
-     * 
-     * @param x
-     */
-    public void setX(Unit x)
-    {
-        this.x = x;
-    }
-
-    /**
-     * Sets the Y position.
-     * 
-     * @param y
-     */
-    public void setY(Unit y)
-    {
-        this.y = y;
     }
 
     /**
@@ -130,36 +98,62 @@ public abstract class GameObject
 
     public Unit getX()
     {
-        return this.x;
+        return Unit.forUnits(this.transform.getTranslationX() * Unit.getRatio());
     }
 
     public Unit getY()
     {
-        return this.y;
+        return Unit.forUnits(this.transform.getTranslationY() * Unit.getRatio());
+    }
+
+    public Unit getCenterX()
+    {
+        return Unit.forUnits(this.getWorldCenter().x * Unit.getRatio());
+    }
+
+    public Unit getCenterY()
+    {
+        return Unit.forUnits(this.getWorldCenter().y * Unit.getRatio());
+    }
+
+    public Unit getLocalCenterX()
+    {
+        return Unit.forUnits(this.getLocalCenter().x * Unit.getRatio());
+    }
+
+    public Unit getLocalCenterY()
+    {
+        return Unit.forUnits(this.getLocalCenter().y * Unit.getRatio());
+    }
+
+    public double getRotation()
+    {
+        return this.transform.getRotation();
+    }
+
+    public void setVelocity(double x, double y)
+    {
+        setLinearVelocity(x, y);
+        setAsleep(false);
+    }
+
+    public void setVelocity(Vector2 v)
+    {
+        setVelocity(v.x, v.y);
+    }
+
+    public void setVelocityX(double x)
+    {
+        setVelocity(x, getLinearVelocity().y);
+    }
+
+    public void setVelocityY(double y)
+    {
+        setVelocity(getLinearVelocity().x, y);
     }
 
     public Unit getZ()
     {
         return this.z;
-    }
-
-    public void setXVelocity(Unit xVel)
-    {
-        this.xVel = xVel;
-    }
-
-    public void setYVelocity(Unit yVel)
-    {
-        this.yVel = yVel;
-    }
-
-    public Unit getXVelocity()
-    {
-        return this.xVel;
-    }
-
-    public Unit getYVelocity()
-    {
-        return this.yVel;
     }
 }
