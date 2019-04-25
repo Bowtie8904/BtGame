ad2435f9442291ab3ec87f51a1f1a40b6fae11d0
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-04-26 00:33:56 +0200

    Defined onCollision and getBody to receive collision information.

--- src/bt/game/core/obj/col/Collider.java
+++ src/bt/game/core/obj/col/Collider.java
@@ -1,10 +1,17 @@
 package bt.game.core.obj.col;
 
+import org.dyn4j.collision.narrowphase.Penetration;
+import org.dyn4j.dynamics.Body;
+import org.dyn4j.dynamics.BodyFixture;
+
 /**
  * @author &#8904
  *
  */
 public interface Collider
 {
+    public boolean onCollision(Body body1, BodyFixture fixture1, Body body2, BodyFixture fixture2,
+            Penetration penetration);
 
+    public Body getBody();
 }
