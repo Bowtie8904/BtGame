506dbbe0aef5623e11a4f981bb35b7bc698d7cf6
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-04-27 01:08:08 +0200

    Moved onCVollide to deifferent sub-interfaces.

--- src/bt/game/core/obj/col/Collider.java
+++ src/bt/game/core/obj/col/Collider.java
@@ -1,8 +1,6 @@
 package bt.game.core.obj.col;
 
-import org.dyn4j.collision.narrowphase.Penetration;
 import org.dyn4j.dynamics.Body;
-import org.dyn4j.dynamics.BodyFixture;
 
 /**
  * @author &#8904
@@ -10,8 +8,5 @@ import org.dyn4j.dynamics.BodyFixture;
  */
 public interface Collider
 {
-    public boolean onCollision(Body body1, BodyFixture fixture1, Body body2, BodyFixture fixture2,
-            Penetration penetration);
-
     public Body getBody();
 }
