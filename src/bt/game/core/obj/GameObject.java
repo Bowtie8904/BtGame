dad7c7de86ac7ac44cf8dd0cfa1a8cb4cd79ff71
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-04-28 19:43:34 +0200

    Converted to interface.

--- src/bt/game/core/obj/GameObject.java
+++ src/bt/game/core/obj/GameObject.java
@@ -1,104 +1,49 @@
 package bt.game.core.obj;
 
-import bt.game.core.obj.hand.ObjectHandler;
 import bt.game.core.scene.Scene;
 import bt.game.util.unit.Unit;
 
 /**
- * An abstract base class for all objects that have a position in the game and need to be handled by an
- * {@link ObjectHandler}, i.e. characters in a scene.
+ * A base interface for all objects that have a position in the game, i.e. characters in a scene.
  * 
  * @author &#8904
  */
-public abstract class GameObject
+public interface GameObject
 {
-    /** The unit X position of this object. */
-    protected Unit x;
-
-    /** The unit Y position of this object. */
-    protected Unit y;
-
-    /** The unit width of this object. */
-    protected Unit w;
-
-    /** The unit height of this object. */
-    protected Unit h;
-
-    /** The scene that this object is used in. */
-    protected Scene scene;
-
-    /**
-     * Creates a new instance and initializes its fields.
-     * 
-     * @param scene
-     *            The scene that this object is created for.
-     * @param z
-     *            The Z position.
-     */
-    public GameObject(Scene scene, Unit x, Unit y)
-    {
-        this.scene = scene;
-        this.x = x;
-        this.y = y;
-    }
-
     /**
      * Gets the scene that this object was created for.
      * 
      * @return
      */
-    public Scene getScene()
-    {
-        return this.scene;
-    }
+    public Scene getScene();
 
     /**
      * Sets the width of this object.
      * 
      * @param w
      */
-    public void setW(Unit w)
-    {
-        this.w = w;
-    }
+    public void setW(Unit w);
 
     /**
      * Sets the height of this object.
      * 
      * @param h
      */
-    public void setH(Unit h)
-    {
-        this.h = h;
-    }
+    public void setH(Unit h);
 
-    public Unit getH()
-    {
-        return this.h;
-    }
+    public Unit getH();
 
-    public Unit getW()
-    {
-        return this.w;
-    }
+    public Unit getW();
 
-    public Unit getX()
-    {
-        return this.x;
-    }
+    public Unit getX();
 
-    public Unit getY()
-    {
-        return this.y;
-    }
+    public Unit getY();
 
-    public void setX(Unit x)
-    {
-        this.x = x;
-    }
+    public void setX(Unit x);
 
-    public void setY(Unit y)
-    {
-        this.y = y;
-    }
+    public void setY(Unit y);
+
+    public Unit getCenterX();
+
+    public Unit getCenterY();
 }
