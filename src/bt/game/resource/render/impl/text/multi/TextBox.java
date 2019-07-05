aa50db4ef7e698068c421b18b170f8da75de44dd
Bowtie8904 <30761922+Bowtie8904@users.noreply.github.com>
2019-07-06 00:12:20 +0200

    Yes, i am super motivated to do this.

---
+++ src/bt/game/resource/render/impl/text/multi/TextBox.java
@@ -0,0 +1,12 @@
+package bt.game.resource.render.impl.text.multi;
+
+import bt.game.resource.render.Renderable;
+
+/**
+ * @author &#8904
+ *
+ */
+public interface TextBox extends Renderable
+{
+
+}
